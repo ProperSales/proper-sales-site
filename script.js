d9808b719c740e7393ac5cba0b9cf74df7e81512
ProperSales <greg@proper-sales.co.uk>
2025-06-10 17:43:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 <script src="script.js"></script>
 </body>
 </html>
+
+// Ping-pong loop for hero video
+const heroVid = document.getElementById('hero-video');
+
+// When playback naturally ends...
+heroVid.addEventListener('ended', () => {
+  // Flip direction: if playing forward, go backwards; if backwards, go forward
+  heroVid.playbackRate = heroVid.playbackRate > 0 ? -1 : 1;
+  // Restart playback at whichever end we’re on
+  heroVid.play();
+});
